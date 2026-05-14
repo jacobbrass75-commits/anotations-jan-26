bab3e194ada298e10ef78d021575de440e99a6ae
jacobbrass75-commits <jacobbrass75-commits@users.noreply.github.com>
2026-05-14 19:40:08 -0400

    Fix deploy wrapper sudo env handling

--- deploy/deploy-prod.sh
+++ deploy/deploy-prod.sh
@@ -5,7 +5,14 @@ DEPLOY_HOST="${DEPLOY_HOST:-deploy@89.167.10.34}"
 APP_DIR="${APP_DIR:-/opt/app}"
 APP_REF="${APP_REF:-origin/master}"
 SKIP_PREDEPLOY_BACKUP="${SKIP_PREDEPLOY_BACKUP:-0}"
-REMOTE_COMMAND="cd '$APP_DIR' && sudo -n APP_DIR='$APP_DIR' APP_REF='$APP_REF' SKIP_PREDEPLOY_BACKUP='$SKIP_PREDEPLOY_BACKUP' /bin/bash '$APP_DIR/deploy/refresh-prod.sh'"
+printf -v REMOTE_INNER \
+  "cd %q && APP_DIR=%q APP_REF=%q SKIP_PREDEPLOY_BACKUP=%q /bin/bash %q" \
+  "$APP_DIR" \
+  "$APP_DIR" \
+  "$APP_REF" \
+  "$SKIP_PREDEPLOY_BACKUP" \
+  "$APP_DIR/deploy/refresh-prod.sh"
+REMOTE_COMMAND="sudo -n /bin/bash -lc $(printf "%q" "$REMOTE_INNER")"
 
 ssh \
   -o BatchMode=yes \
